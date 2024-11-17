78d3463b55ec56f395bffb339e87d3c9aa31c388
antonia2003-byte <t8200168@aueb.gr>
2024-11-17 17:31:42 +0200

    Update Assignment1.java

--- Assignment1.java
+++ Assignment1.java
@@ -2,5 +2,9 @@ public class Assignment1 {
     public static void main(String[] args) {
         System.out.println("ΑM:8220016");
         System.out.println("ΑΜ:8230022");
+        System.out.println("AM:8200168");
+        
+        
+        
     }
 }
